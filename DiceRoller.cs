3a377aa89b0348ab02443086a012d1aa140c1f26
markbarlocker <markbarlocker@MARKBARLOCK701C>
2024-01-17 17:37:08 -0700

    Add project files.

---
+++ DiceRoller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class DiceRoller
+    {
+        public int[] rollTheDice(int numRolls)
+        {
+            // make my dice variables
+            int dice1;
+            int dice2;
+            int sum;
+
+            // make array to hold count of each number rolled
+            int[] results = new int[13];
+
+            // Create random as an object
+            Random rand = new Random();
+
+            // loop through each roll
+            for (int i = 0; i < numRolls; i++)
+            {
+                // roll my dice
+                dice1 = rand.Next(1, 7);
+                dice2 = rand.Next(1, 7);
+                sum = dice1 + dice2;
+
+                results[sum]++;
+            }
+            return results;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Mission2;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DiceRoller dr = new DiceRoller();
+
+        // Ask for the numbner of rolls the user would like to do
+        int numRolls = 0;
+        Console.WriteLine("Welcome to the dice throwing simulator! ");
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+        numRolls = int.Parse(Console.ReadLine());
+
+        // Call the rollTheDice method and assign it to results
+        int[] results = dr.rollTheDice(numRolls);
+
+        // Print more things to the console
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine("Total number of rolls = " + numRolls + ".");
+        Console.WriteLine();
+
+        // print the results as a histogram
+        // loop through the results and display as histogram with '*'
+        int percent;
+        for (int i = 2; i <= 12; i++)
+        {
+            percent = results[i] * 100 / numRolls;
+            Console.WriteLine($"{i}: {new string('*', percent)}");
+        }
+    }
+}
